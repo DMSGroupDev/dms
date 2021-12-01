4fd457767dff10edf5153c74bf0a73acd8c3d77e
Martin Hejna <martin.hejna@icnet.eu>
2021-12-01 11:07:20 +0100

    Forgoten password validace na 24 hodin.

--- src/dms-backend-api/dms-backend-api/Services/Identity/UserTokenProvider.cs
+++ src/dms-backend-api/dms-backend-api/Services/Identity/UserTokenProvider.cs
@@ -29,14 +29,13 @@ namespace dms_backend_api.Services.Identity
             return Task.FromResult(false);
         }
 
-        // Genereates a simple token based on the user id, email and another string.
         private string GenerateToken(ApplicationUser user, string purpose)
         {
             string secretString = (string)_configuration.GetValue(typeof(string), "JWTSecret");
             if (string.IsNullOrEmpty(secretString))
                 throw new InvalidOperationException("The JWT Secret is empty.");
 
-            return secretString + user.Email + purpose + user.Id;
+            return secretString + user.Email + purpose + user.Id + "|?t=" + ConvertToTimestamp(DateTime.UtcNow);
         }
 
         public Task<string> GenerateAsync(string purpose, UserManager<TUser> manager, TUser user)
@@ -46,8 +45,32 @@ namespace dms_backend_api.Services.Identity
 
         public Task<bool> ValidateAsync(string purpose, string token, UserManager<TUser> manager, TUser user)
         {
-            return Task.FromResult(token == GenerateToken(user, purpose));
+            if (long.TryParse(token.Substring(token.IndexOf("|?t=") + 4), out long parsedDate))
+            {
+                DateTime date = UnixTimeStampToDateTime(parsedDate);
+                if (date > DateTime.UtcNow.AddHours(-24) && date <= DateTime.UtcNow)
+                    return Task.FromResult(token == GenerateToken(user, purpose));
+            }
+            return Task.FromResult(false);
         }
+
+        #region Timestamps
+        
+        internal static long ConvertToTimestamp(DateTime value)
+        {
+            TimeSpan elapsedTime = value - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return (long)elapsedTime.TotalSeconds;
+        }
+
+        internal static DateTime UnixTimeStampToDateTime(long unixTimeStamp)
+        {
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp);
+            return dateTime;
+        }
+        
+        #endregion
+        
         #endregion
     }
 }
